80e164ba042543a451c4632a24120fbb3621a95c
Peremobowei <81290841+Peremobowei@users.noreply.github.com>
2022-12-16 18:00:15 +0100

    Add files via upload

---
+++ banking-landing page/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// // btn1
+// document.querySelector(".btntab1").addEventListener("click", function () {
+//   document.querySelector(".tab2").classList.add("displayNone");
+//   document.querySelector(".tab3").classList.add("displayNone");
+// });
+
+// // btn2
+// document.querySelector(".btntab2").addEventListener("click", function () {
+//   document.querySelector(".tab1").classList.add("displayNone");
+//   document.querySelector(".tab3").classList.add("displayNone");
+//   document.querySelector(".btntab1").classList.remove("border-bottom");
+//   document.querySelector(".btntab2").classList.add("border-bottom");
+// });
+
+// // btn3
+// document.querySelector(".btntab3").addEventListener("click", function () {
+//   document.querySelector(".tab1").classList.add("displayNone");
+//   document.querySelector(".tab2").classList.add("displayNone");
+// });
